7e0f7eaf9b06a630e5abfeb17f667c14e4c4121a
caiosweet <24454580+caiosweet@users.noreply.github.com>
2020-05-21 22:55:44 +0200

    Add files via upload

--- custom_components/dpc/binary_sensor.py
+++ custom_components/dpc/binary_sensor.py
@@ -1,167 +1,187 @@
-"""Binary Sensor for Protezione Civile"""
+"""Binary Sensor for Dipartimento della Protezione Civile Meteo-Hydro Alert"""
 import aiohttp
+import asyncio
+
+# import async_timeout # TODO#
 from datetime import date, timedelta
 from dateutil.parser import parse
-import asyncio
-# import async_timeout #TODO#
+import feedparser
 import json
 import logging
 
 import voluptuous as vol
 from homeassistant.components.binary_sensor import PLATFORM_SCHEMA, ENTITY_ID_FORMAT
+
+try:
+    from homeassistant.components.binary_sensor import BinarySensorEntity
+except ImportError:
+    from homeassistant.components.binary_sensor import (
+        BinarySensorDevice as BinarySensorEntity,
+    )
 from homeassistant.const import CONF_NAME, ATTR_ATTRIBUTION, CONF_SCAN_INTERVAL
-# from homeassistant.helpers.aiohttp_client import async_get_clientsession #TODO#
+
+# from homeassistant.helpers.aiohttp_client import async_get_clientsession # TODO#
 import homeassistant.helpers.config_validation as cv
-from homeassistant.components.binary_sensor import BinarySensorDevice
 from homeassistant.helpers.entity import async_generate_entity_id
 from homeassistant.util import Throttle
 
-_LOGGER = logging.getLogger(__name__)
+from .const import (
+    ATTRIBUTION,
+    BASE_URL,
+    FEED_URL,
+    CONF_ALERT,
+    CONF_ISTAT,
+    CONF_WARNINGS,
+    DEFAULT_DEVICE_CLASS,
+    DEFAULT_NAME,
+    WARNING_TYPES,
+    WARNING_ALERT,
+)
 
-ATTRIBUTION = 'Information provided by protezionecivilepop.tk'
-
-CONF_ALERT = 'alert'
-CONF_ISTAT = 'istat'
-CONF_WARNINGS = 'warnings'
-
-DEFAULT_DEVICE_CLASS = 'safety'
-DEFAULT_NAME = 'dpc'
 DEFAULT_SCAN_INTERVAL = timedelta(minutes=30)
 
-WARNING_TYPES = {
-    'temporali_oggi': ['temporali_oggi', 'Rischio Temporali Oggi', 'mdi:weather-lightning'],
-    'idraulico_oggi': ['idraulico_oggi', 'Rischio Idraulico Oggi', 'mdi:home-flood'],
-    'idrogeologico_oggi': ['idrogeologico_oggi', 'Rischio Idrogeologico Oggi', 'mdi:waves'],
-    'temporali_domani': ['temporali_domani', 'Rischio Temporali Domani', 'mdi:weather-lightning'],
-    'idraulico_domani': ['idraulico_domani', 'Rischio Idraulico Domani', 'mdi:home-flood'],
-    'idrogeologico_domani': ['idrogeologico_domani', 'Rischio Idrogeologico Domani', 'mdi:waves'],
-}
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Required(CONF_ISTAT): cv.string,
+        vol.Optional(CONF_ALERT, default="GIALLA"): cv.string,
+        vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+        vol.Optional(CONF_WARNINGS, default=[]): vol.All(
+            cv.ensure_list, [vol.In(WARNING_TYPES)]
+        ),
+        vol.Optional(CONF_SCAN_INTERVAL, default=DEFAULT_SCAN_INTERVAL): cv.time_period,
+    }
+)
 
-WARNING_ALERT = {
-    'BIANCA' : 0,
-    'VERDE': 1,
-    'GIALLA': 2,
-    'ARANCIONE': 3,
-    'ROSSA': 4,
-}
+_LOGGER = logging.getLogger(__name__)
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_ISTAT): cv.string,
-    vol.Optional(CONF_ALERT, default='GIALLA'): cv.string,
-    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-    vol.Optional(CONF_WARNINGS, default=[]):
-        vol.All(cv.ensure_list, [vol.In(WARNING_TYPES)]),
-    vol.Optional(CONF_SCAN_INTERVAL, default=DEFAULT_SCAN_INTERVAL): cv.time_period
-})
 
+@asyncio.coroutine
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    """Setup."""
+    _LOGGER.debug("start async setup platform")
     name = config.get(CONF_NAME)
     warnings = config.get(CONF_WARNINGS)
     istat = str(config.get(CONF_ISTAT)).zfill(6)
     alert = config.get(CONF_ALERT)
     scan_interval = config.get(CONF_SCAN_INTERVAL)
     sensors = []
-    sensor_name = '{} - '.format(name)
-    updater = dpcUpdater(hass, istat, scan_interval)
+    updater = dpcUpdater(hass, istat, scan_interval, warnings)
     await updater.async_update()
     for warning_type in warnings:
-        uid = '{}_{}'.format(name, warning_type)
+        uid = f"{name}_{warning_type}"
         entity_id = async_generate_entity_id(ENTITY_ID_FORMAT, uid, hass=hass)
-        sensors.append(dpcSensor(entity_id, sensor_name, updater, warning_type, alert))
-    _LOGGER.debug('UPDATER: %s', updater.dpc_output)
+        sensors.append(dpcSensor(entity_id, name, updater, warning_type, alert))
     async_add_entities(sensors, True)
 
-class dpcSensor(BinarySensorDevice):
+
+class dpcSensor(BinarySensorEntity):
+    """Binary sensor representing Meteo-Hydro Alert data."""
+
     def __init__(self, entity_id, name, updater, warning_type, alert):
+        """Initialize dpc binary sensor."""
         self.entity_id = entity_id
         self._name = name
         self._updater = updater
-        self._data = None
         self._warning_type = warning_type
         self._warning_key = WARNING_TYPES[self._warning_type][0]
         self._alert = WARNING_ALERT.get(alert.upper())
 
     @property
     def is_on(self):
-        data = self._updater.dpc_output[self._warning_key]
-        k_date = parse(data['date']).date()
-        return data is not None and WARNING_ALERT.get(data['alert']) >= self._alert and k_date >= date.today()
+        try:
+            data = self._updater.dpc_output[self._warning_key]
+            prevision_date = parse(data["date"]).date()
+        except KeyError:
+            return False
+        return (
+            data is not None
+            and WARNING_ALERT.get(data["alert"]) >= self._alert
+            and prevision_date >= date.today()
+        )
 
     @property
     def device_state_attributes(self):
+        """Return attrubutes."""
         output = dict()
         output[ATTR_ATTRIBUTION] = ATTRIBUTION
+        if self._updater.newlink:
+            output["link"] = self._updater.newlink
         if self.is_on:
             data = self._updater.dpc_output[self._warning_key]
-            output['data'] = parse(data['date']).date().strftime('%d-%m-%Y')
-            output['rischio'] = data['risk'].capitalize()
-            output['info'] = data['info']
-            output['allerta'] = data['alert']
-            output['istat'] = data['istat_code']
-            output['città'] = data['city_name']
-            output['provincia'] = data['province_name']
-            output['prov'] = data['province_code']
-            output['regione'] = data['region']
-            output['zona_id'] = data['civil_protection_zone_id']
-            output['zona_info'] = data['civil_protection_zone_info']
-            output['longitudine'] = data['longitude']
-            output['latitudine'] = data['latitude']
-            output['level'] = WARNING_ALERT.get(data['alert'])
+            output["data"] = parse(data["date"]).date().strftime("%d-%m-%Y")
+            output["rischio"] = data["risk"].capitalize()
+            output["info"] = data["info"]
+            output["allerta"] = data["alert"]
+            output["istat"] = data["istat_code"]
+            output["città"] = data["city_name"]
+            output["provincia"] = data["province_name"]
+            output["prov"] = data["province_code"]
+            output["regione"] = data["region"]
+            output["zona_id"] = data["civil_protection_zone_id"]
+            output["zona_info"] = data["civil_protection_zone_info"]
+            output["longitudine"] = data["longitude"]
+            output["latitudine"] = data["latitude"]
+            output["level"] = WARNING_ALERT.get(data["alert"])
+            output["link"] = data["link"]
         return output
 
     @property
     def device_class(self):
+        """Return device_class."""
         return DEFAULT_DEVICE_CLASS
 
     @property
     def icon(self):
+        """Return icon."""
         return WARNING_TYPES[self._warning_type][2]
 
     @property
     def name(self):
+        """Return name."""
         return WARNING_TYPES[self._warning_type][1]
 
-    @property
-    def available(self) -> bool:
-        """Return True if entity is available."""
-        try:
-            info = self._updater.dpc_output[self._warning_key]['info']
-        except KeyError:
-            return False
-        return True
-
+    # @asyncio.coroutine
     async def async_update(self):
+        """Update data."""
         await self._updater.async_update()
 
+
 class dpcUpdater:
-    def __init__(self, hass, istat, scan_interval):
-        # self._hass = hass #TODO#
+    """fetch all data."""
+
+    def __init__(self, hass, istat, scan_interval, warnings):
+        self._hass = hass
         self._istat = istat
-        self.dpc_output = {}
-        self.async_update = Throttle(scan_interval)(self._async_update)
+        self._warnings = warnings
+        self.dpc_output = None
+        self.newlink = None
+        self.async_update = Throttle(scan_interval)(self.async_update)
 
-    async def _async_update(self):
-        url_base = f"http://www.protezionecivilepop.tk/allerte?citta={self._istat}&rischio="
-        URL_LIST = [
-            url_base + 'temporali' + '&allerta=' + 'verde' + '&giorno=' + 'domani' + '&formato=json',
-            url_base + 'idraulico' + '&allerta=' + 'verde' + '&giorno=' + 'domani' + '&formato=json',
-            url_base + 'idrogeologico' + '&allerta=' + 'verde' + '&giorno=' + 'domani' + '&formato=json',
-            url_base + 'temporali' + '&allerta=' + 'verde' + '&giorno=' + 'oggi' + '&formato=json',
-            url_base + 'idraulico' + '&allerta=' + 'verde' + '&giorno=' + 'oggi' + '&formato=json',
-            url_base + 'idrogeologico' + '&allerta=' + 'verde' + '&giorno=' + 'oggi' + '&formato=json'
-        ]
-        await self.fetch_all(URL_LIST)
-        # await self._hass.async_add_executor_job(self.fetch_all) #TODO?#
+    async def async_update(self):
+        await self._hass.async_add_executor_job(self.new_feed)
+        self.dpc_output = await self.fetch_all()
+        _LOGGER.debug("ASYNC UPDATE: %s\n", self.dpc_output)
 
-    async def fetch_all(self, urls):
+    def new_feed(self):
+        try:
+            NewsFeed = feedparser.parse(FEED_URL)
+            entry = NewsFeed.entries[0]
+            self.newlink = entry.link
+        except:
+            _LOGGER.error("NEW LINK DATE: %s \n", self.newlink)
+            self.newlink = None
+
+    async def fetch_all(self):
         """Launch requests for all url."""
         tasks = []
         async with aiohttp.ClientSession() as session:
-            for url in urls:
+            for warning_type in self._warnings:
+                warning_type = warning_type.split("_")
+                url = BASE_URL.format(self._istat, warning_type[0], warning_type[1])
                 task = asyncio.ensure_future(self.fetch(url, session))
                 tasks.append(task)
-            responses = await asyncio.gather(*tasks)
-            _LOGGER.debug('RESPONSE: %s', responses)
+            dataList = await asyncio.gather(*tasks)
+            responses = {k: v for element in dataList for k, v in element.items()}
             return responses
 
     async def fetch(self, url, session):
@@ -169,28 +189,43 @@ class dpcUpdater:
         try:
             async with session.get(url, timeout=10) as response:
                 if response.status != 200:
-                    _LOGGER.error('Connection failed with http code: %s with url: %s', response.status, url)
+                    _LOGGER.error(
+                        "Connection failed with http code: %s with url: %s",
+                        response.status,
+                        url,
+                    )
                     return {}
                 try:
                     data = await response.json()
                 except aiohttp.ContentTypeError:
                     # If json decoder could not parse the response
-                    _LOGGER.error('Failed to parse response from site. Check the istat number: %s - URL: %s', self._istat, url)
+                    _LOGGER.error(
+                        "Failed to parse response from site. Check the istat number: %s - URL: %s",
+                        self._istat,
+                        url,
+                    )
                     return {}
-
+                jsondata = {}
                 # Parsing response
-                k = data['previsione']
-                k_date = parse(k['date']).date()
-                if k_date == date.today():
-                    self.dpc_output[k['risk'] + '_oggi'] = k
-                elif k_date > date.today():
-                    self.dpc_output[k['risk'] + '_domani'] = k
-                else:
-                    day = '_domani' if 'oggi' in url and k_date < date.today() + timedelta(days=1) else '_oggi'
-                    self.dpc_output[k['risk'] + day] = {}
-                _LOGGER.debug('DATA response: %s\n', k)
-                json.loads(json.dumps(self.dpc_output))
-                return url, data
+                prevision = data.get("previsione", {})  # data["previsione"]
+                prevision_date = parse(prevision["date"]).date()
+
+                if self.newlink:
+                    prevision["link"] = self.newlink
+                if prevision_date == date.today():
+                    jsondata[prevision["risk"] + "_oggi"] = prevision
+                elif prevision_date > date.today():
+                    jsondata[prevision["risk"] + "_domani"] = prevision
+                else:  # prevision_date < date.today():
+                    day = (
+                        "_domani"
+                        if "oggi" in url
+                        and prevision_date < date.today() + timedelta(days=1)
+                        else "_oggi"
+                    )
+                    jsondata[prevision["risk"] + day] = {"link": prevision["link"]}
+                _LOGGER.debug("JSON DATA: %s\n", jsondata)
+                return json.loads(json.dumps(jsondata))
         except aiohttp.client_exceptions.ClientConnectorError:
-            _LOGGER.error('Cannot connect to: %s', url)
+            _LOGGER.error("Cannot connect to: %s", url)
             return {}
---
+++ custom_components/dpc/const.py
@@ -0,0 +1,50 @@
+"""Constants"""
+
+ATTRIBUTION = "Information provided by protezionecivilepop.tk"
+
+BASE_URL = "http://www.protezionecivilepop.tk/allerte?citta={}&rischio={}&allerta=verde&giorno={}&formato=json"
+FEED_URL = "http://www.protezionecivile.gov.it/web/guest/dettaglio/-/journal/rss/351565?doAsGroupId=20182&refererPlid=42041&controlPanelCategory=current_site.content&_15_groupId=20182"
+
+CONF_ALERT = "alert"
+CONF_ISTAT = "istat"
+CONF_WARNINGS = "warnings"
+
+DEFAULT_DEVICE_CLASS = "safety"
+DEFAULT_NAME = "dpc"
+
+WARNING_TYPES = {
+    "temporali_oggi": [
+        "temporali_oggi",
+        "Rischio Temporali Oggi",
+        "mdi:weather-lightning",
+    ],
+    "idraulico_oggi": ["idraulico_oggi", "Rischio Idraulico Oggi", "mdi:home-flood"],
+    "idrogeologico_oggi": [
+        "idrogeologico_oggi",
+        "Rischio Idrogeologico Oggi",
+        "mdi:waves",
+    ],
+    "temporali_domani": [
+        "temporali_domani",
+        "Rischio Temporali Domani",
+        "mdi:weather-lightning",
+    ],
+    "idraulico_domani": [
+        "idraulico_domani",
+        "Rischio Idraulico Domani",
+        "mdi:home-flood",
+    ],
+    "idrogeologico_domani": [
+        "idrogeologico_domani",
+        "Rischio Idrogeologico Domani",
+        "mdi:waves",
+    ],
+}
+
+WARNING_ALERT = {
+    "BIANCA": 0,
+    "VERDE": 1,
+    "GIALLA": 2,
+    "ARANCIONE": 3,
+    "ROSSA": 4,
+}
