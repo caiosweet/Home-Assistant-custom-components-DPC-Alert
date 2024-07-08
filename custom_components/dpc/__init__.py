524edcc0fad47f1e270f4c132486d3182f69a854
caiosweet <24454580+caiosweet@users.noreply.github.com>
2024-07-08 05:17:30 +0200

    Update __init__.py

--- custom_components/dpc/__init__.py
+++ custom_components/dpc/__init__.py
@@ -70,7 +70,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     for platform in PLATFORMS:
         if entry.options.get(platform, True):
             coordinator.platforms.append(platform)
-            await hass.config_entries.async_forward_entry_setup(entry, platform)
+            await hass.config_entries.async_forward_entry_setups(entry, [platform])
 
     if not entry.update_listeners:
         entry.add_update_listener(async_reload_entry)
